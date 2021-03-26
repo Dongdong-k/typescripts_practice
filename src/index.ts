fdbf31811a1fc9382df66dcc561e69bb0bc16fcb
dongdong <crisish@naver.com>
2021-03-26 16:41:13 +0900

    객체와 인터페이스 사용 바법

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-const name: string = "dongdong",
-  age: number = 32,
-  gender: string = "male";
+// 객체와 인터페이스의 구조는 동일해야함
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
 
-// gender? 처럼 변수에 ? 표시를 해주는 것은 필수요소는 아니라는 의미
-// function () => : voide {} , void 의미는 함수의 리턴 값을 아무것도 주지 않겠다는 의미, 함수 리턴값도 유형 선택가능
-const sayHi = (name: string, age: number, gender?: string): void => {
+const person = {
+  name: "dongdong",
+  age: 30,
+  gender: "male",
+};
+
+const sayHi = (person: Human): void => {
   console.log(
-    `my name is ${name} and ${age} years old and my gender ${gender}`
+    `my name is ${person.name} and ${person.age} years old and my gender ${person.gender}`
   );
 };
 
-sayHi(name, age, gender);
+sayHi(person);
 
 export {};
